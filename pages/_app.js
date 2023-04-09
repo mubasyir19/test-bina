a721cbe13daed114188fed5ec022ac3dec8bbdfd
mubasyir19 <m.mubasyir19@gmail.com>
2023-04-09 18:35:25 +0700

    Big Edit

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,4 @@
 import Head from 'next/head';
-import '../assets/css/nucleo-icons.css';
-import '../assets/css/nucleo-svg.css';
-import '../assets/css/soft-design-system.css';
-// import '../styles/soft-design-system.css.map';
-import '../assets/css/soft-design-system.min.css';
-// import '../assets/js/core/popper.min';
-// import '../assets/js/core/bootstrap.min';
-// import
 
 export default function App({ Component, pageProps }) {
   return (
@@ -16,52 +8,74 @@ export default function App({ Component, pageProps }) {
         <link href='https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700' rel='stylesheet' />
 
         {/* <!-- Nucleo Icons --> */}
-        <link href='./assets/css/nucleo-icons.css' rel='stylesheet' />
-        <link href='./assets/css/nucleo-svg.css' rel='stylesheet' />
+        <link href='/css/nucleo-icons.css' rel='stylesheet' />
+        <link href='/css/nucleo-svg.css' rel='stylesheet' />
 
         {/* <!-- Font Awesome Icons --> */}
         <script src='https://kit.fontawesome.com/42d5adcbca.js' crossorigin='anonymous'></script>
-        <link href='./assets/css/nucleo-svg.css' rel='stylesheet' />
+        <link href='/css/nucleo-svg.css' rel='stylesheet' />
 
         {/* <!-- CSS Files --> */}
-        <link id='pagestyle' href='./assets/css/soft-design-system.css?v=1.0.9' rel='stylesheet' />
 
-        {/* <!-- Nucleo Icons --> */}
-        <link
-          href='https://unpkg.com/soft-ui-design-system@1.0.4/assets/css/soft-design-system.min.css'
-          rel='stylesheet'
-        />
+        <link id='pagestyle' href='/css/soft-design-system.css?v=1.0.9' rel='stylesheet' />
+
+        {/* <!--   Core JS Files   --> */}
+        <script src='/js/core/popper.min.js' type='text/javascript'></script>
+        <script src='/js/core/bootstrap.min.js' type='text/javascript'></script>
+        <script src='/js/plugins/perfect-scrollbar.min.js'></script>
+
+        {/* <!--  Plugin for TypedJS, full documentation here: https://github.com/inorganik/CountUp.js --> */}
+        <script src='/js/plugins/countup.min.js'></script>
+
+        <script src='/js/plugins/choices.min.js'></script>
+
+        <script src='/js/plugins/prism.min.js'></script>
+        <script src='/js/plugins/highlight.min.js'></script>
+
+        {/* <!--  Plugin for Parallax, full documentation here: https://github.com/dixonandmoe/rellax --> */}
+        <script src='/js/plugins/rellax.min.js'></script>
+        {/* <!--  Plugin for TiltJS, full documentation here: https://gijsroge.github.io/tilt.js/ --> */}
+        <script src='/js/plugins/tilt.min.js'></script>
+        {/* <!--  Plugin for Selectpicker - ChoicesJS, full documentation here: https://github.com/jshjohnson/Choices --> */}
+        <script src='/js/plugins/choices.min.js'></script>
+
+        {/* <!--  Plugin for Parallax, full documentation here: https://github.com/wagerfield/parallax  --> */}
+        <script src='/js/plugins/parallax.min.js'></script>
+
+        {/* <!-- Control Center for Soft UI Kit: parallax effects, scripts for the example pages etc --> */}
+        {/* <!--  Google Maps Plugin    --> */}
+
+        <script src='https://maps.googleapis.com/maps/api/js?key=AIzaSyDTTfWur0PDbZWPr7Pmq8K3jiDp0_xUziI'></script>
+        <script src='/js/soft-design-system.min.js?v=1.0.9' type='text/javascript'></script>
+
+        {/* <script type="text/javascript">
+      if (document.getElementById('state1')) {
+        const countUp = new CountUp('state1', document.getElementById('state1').getAttribute('countTo'));
+        if (!countUp.error) {
+          countUp.start();
+        } else {
+          console.error(countUp.error);
+        }
+      }
+      if (document.getElementById('state2')) {
+        const countUp1 = new CountUp('state2', document.getElementById('state2').getAttribute('countTo'));
+        if (!countUp1.error) {
+          countUp1.start();
+        } else {
+          console.error(countUp1.error);
+        }
+      }
+      if (document.getElementById('state3')) {
+        const countUp2 = new CountUp('state3', document.getElementById('state3').getAttribute('countTo'));
+        if (!countUp2.error) {
+          countUp2.start();
+        } else {
+          console.error(countUp2.error);
+        }
+      }
+    </script> */}
       </Head>
       <Component {...pageProps} />
-
-      {/* <!--   Core JS Files   --> */}
-      <script src='../assets/js/core/popper.min.js' type='text/javascript'></script>
-      <script src='../assets/js/core/bootstrap.min.js' type='text/javascript'></script>
-      <script src='../assets/js/plugins/perfect-scrollbar.min.js'></script>
-
-      {/* <!--  Plugin for TypedJS, full documentation here: https://github.com/inorganik/CountUp.js --> */}
-      <script src='../assets/js/plugins/countup.min.js'></script>
-
-      <script src='../assets/js/plugins/choices.min.js'></script>
-
-      <script src='../assets/js/plugins/prism.min.js'></script>
-      <script src='../assets/js/plugins/highlight.min.js'></script>
-
-      {/* <!--  Plugin for Parallax, full documentation here: https://github.com/dixonandmoe/rellax --> */}
-      <script src='../assets/js/plugins/rellax.min.js'></script>
-      {/* <!--  Plugin for TiltJS, full documentation here: https://gijsroge.github.io/tilt.js/ --> */}
-      <script src='../assets/js/plugins/tilt.min.js'></script>
-      {/* <!--  Plugin for Selectpicker - ChoicesJS, full documentation here: https://github.com/jshjohnson/Choices --> */}
-      <script src='./assets/js/plugins/choices.min.js'></script>
-
-      {/* <!--  Plugin for Parallax, full documentation here: https://github.com/wagerfield/parallax  --> */}
-      <script src='../assets/js/plugins/parallax.min.js'></script>
-
-      {/* <!-- Control Center for Soft UI Kit: parallax effects, scripts for the example pages etc --> */}
-      {/* <!--  Google Maps Plugin    --> */}
-
-      <script src='https://maps.googleapis.com/maps/api/js?key=AIzaSyDTTfWur0PDbZWPr7Pmq8K3jiDp0_xUziI'></script>
-      <script src='../assets/js/soft-design-system.min.js?v=1.0.9' type='text/javascript'></script>
     </>
   );
 }
